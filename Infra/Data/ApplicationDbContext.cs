30104246290ab7a7222175e41cd220525afbf9c6
Edson Junior <edson.junior@mereo.com>
2023-05-24 18:02:33 -0300

    Criando a pasta Infra que será responsável pela conexão com o banco de dados. no arquivo ApplicationDbContext foi sobrescrito um método do próprio entityframework. Esse método cria configurações padrões para salvar dados.

---
+++ Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using FirstProjectDotNetCore.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstProjectDotNetCore.Infra.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options) { }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+       
+    }
+
+    //Conversão padrão Global
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        //Explicação: Todos os campos de string relacionados as classes(Category e Product por exemplo) 
+        //Serão salvos no banco com o valor máximo de 100 caracteres.
+        configurationBuilder.Properties<string>()
+            .HaveMaxLength(100);
+
+
+    }
+}
