eed635e0e00c4bf79f2b39b9da9d6be1ff2c3b66
Edson Junior <edson.junior@mereo.com>
2023-06-09 17:34:44 -0300

    Cria paginação para obter todas as Categorias.

---
+++ Infra/Services/QueryAllCategories.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using FirstProjectDotNetCore.Endpoints.Categories;
+using Microsoft.Data.SqlClient;
+
+namespace FirstProjectDotNetCore.Infra.Services;
+
+public class QueryAllCategories
+{
+    private readonly IConfiguration configuration;
+    public QueryAllCategories(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public async Task<IEnumerable<CategoryResponse>> Execute(int page, int rows)
+    {
+        //criando uma nova conexão com o banco de dados SQL
+        var db = new SqlConnection(configuration["ConnectionStrings:FirstProjectDotNet"]);
+
+        var query = @"SELECT *
+                    FROM Categories
+                    WHERE Categories.Active = 1
+                    Order By Name
+                        OFFSET (@page - 1) * @rows ROWS FETCH NEXT @rows ROWS ONLY";
+
+        return await db.QueryAsync<CategoryResponse>(
+              query,
+              new { page, rows }
+           );
+    }
+}
